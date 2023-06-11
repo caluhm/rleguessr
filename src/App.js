5f7fcd0485e0658957fc2617e2511f4fbd949425
caluhm <calum.jarvo@gmail.com>
2023-06-11 22:24:52 +0100

    removes cookies if present

--- src/App.js
+++ src/App.js
@@ -37,6 +37,19 @@ import {
 import { MAX_CHALLENGES } from './constants/settings';
 import SettingsModal from './components/Modals/SettingsModal';
 
+function deleteAllCookies() {
+  const cookies = document.cookie.split(";");
+
+  for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i];
+      const eqPos = cookie.indexOf("=");
+      const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+      document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+  }
+}
+
+deleteAllCookies();
+
 function App() {
   const isLatestGame = getIsLatestGame()
 
