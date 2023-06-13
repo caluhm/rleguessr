03f723fefea17ff744f0d513767b20705b91a59c
caluhm <calum.jarvo@gmail.com>
2023-06-13 23:09:49 +0100

    banner up top instead

--- src/components/Modals/CookieConsentModal.jsx
+++ src/components/Modals/CookieConsentModal.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CookieConsentModal = ({handleAcceptCookies, handleDeclineCookies, handlePolicyModal}) => {
   return (
     <div className='fixed w-[100vw] h-full min-h-[100vh] top-0 left-0 z-[500] bg-white/10 backdrop-blur-sm flex flex-col justify-start items-center'>
-      <div className='relative mt-auto overflow-hidden sm:p-5 p-3 bg-[#0c101f] w-full h-[10rem] outline-none flex flex-row justify-between drop-shadow-lg'>
+      <div className='relative mb-auto overflow-hidden sm:p-5 p-3 bg-[#0c101f] w-full h-[10rem] outline-none flex flex-row justify-between drop-shadow-lg'>
         <div className='flex h-full w-[70%] sm:text-center text-left items-center justify-center sm:gap-1.5 gap-3 text-white sm:text-base text-sm flex-col'>
             <p>RLE Guessr uses non-essential cookies from Google Analytics to help enhance your experience.</p> 
             <p className='sm:text-sm text-xs font-light'>By clicking "Accept", you consent to the use of cookies. View the <button className='text-indigo-500 underline font-light sm:text-sm text-xs' onClick={handlePolicyModal}>Privacy and Cookie Policy</button> here.</p>
