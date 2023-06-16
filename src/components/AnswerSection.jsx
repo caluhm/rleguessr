b95afa6519f93be248e956801aaab78f91d346ab
caluhm <calum.jarvo@gmail.com>
2023-06-16 19:51:46 +0100

    answer section doesnt change size on mobile

--- src/components/AnswerSection.jsx
+++ src/components/AnswerSection.jsx
@@ -21,13 +21,13 @@ const AnswerSection = ({
 
   return (
     <div 
-        className={`flex w-full sm:h-[6.25rem] h-[5rem] overflow-hidden bg-[#28335a] text-white cursor-pointer rounded-md border-2 ${answerDivCol}`} 
+        className={`flex w-full h-[6.25rem] overflow-hidden bg-[#28335a] text-white cursor-pointer rounded-md border-2 ${answerDivCol}`} 
         onClick={handleClick}
     >
-        <div className='flex flex-col items-center justify-center sm:min-w-[6.25rem] sm:w-[6.25rem] sm:h-[6.25rem] h-[5rem] min-w-[5rem] w-[5rem] bg-indigo-500 mr-6'>
+        <div className='flex flex-col items-center justify-center min-w-[6.25rem] w-[6.25rem] h-[6.25rem] bg-indigo-500 mr-6'>
             <img src={Profile} width={90} height={90} alt={ANSWER_SECTION_IMAGE_ALT_TEXT} className='invert'/>
         </div>
-        <div className='flex flex-col justify-between items-start w-full sm:pt-1.5 sm:pb-2 pt-0 pb-1'>
+        <div className='flex flex-col justify-between items-start w-full pt-1.5 pb-2'>
             <p className='uppercase font-black tracking-normal sm:text-3xl text-2xl p-0 m-0'>{answer?.name}</p>
             <div className='flex flex-row items-center pb-0.5 m-0'>
                 <Flag code={answerRegObj.code} width={30} className='border border-white mr-2.5' title={'Flag of ' + answer.nationality}/>
